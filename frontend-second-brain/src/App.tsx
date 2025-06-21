9b7692751abf4afcaaffc4a340beb581f0cda1a3
Aditya raj <adityaraj987456@gmail.com>
2025-06-21 23:03:37 +0530

    starting frontend part

---
+++ frontend-second-brain/src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "./components/Button"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     
+      <div className="bg-blue-50">  hi there</div>
+
+    <Button></Button>
+
+    </>
+  )
+}
+
+export default App
