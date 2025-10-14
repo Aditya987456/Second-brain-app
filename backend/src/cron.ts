10d9fdce1c829cb75255fa8dd70e52488e973eb6
Aditya raj <adityaraj987456@gmail.com>
2025-10-15 00:22:07 +0530

    updated the project

--- backend/src/cron.ts
+++ backend/src/cron.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { ContentModel } from "./db";
 import { queueFetchContent } from "./worker";
 
-// Every day at 2 AM---> to again try to fetch content 
+// Every day at 2 AM
 cron.schedule("0 16 * * *", async () => {
   const failedContents = await ContentModel.find({ status: { $in: ["pending", "retrying", "failed"] } });
   failedContents.forEach((c) => queueFetchContent(c._id.toString()));
--- frontend-second-brain/src/pages/landingPage.tsx
+++ frontend-second-brain/src/pages/landingPage.tsx
@@ -114,12 +114,3 @@ export default function LandingPage() {
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
