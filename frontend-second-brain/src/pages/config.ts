c46c272d22781a09a1e63f00d2a9faf76ca68e77
Aditya raj <adityaraj987456@gmail.com>
2025-10-15 20:40:08 +0530

    update config.ts , deployed backend on render

--- frontend-second-brain/src/pages/config.ts
+++ frontend-second-brain/src/pages/config.ts
@@ -1,2 +1,4 @@
 //export const BACKEND_URL = "http://localhost:3000"
-export const BACKEND_URL = "https://secondbrainai.up.railway.app"
+//export const BACKEND_URL = "https://secondbrainai.up.railway.app"
+
+export const BACKEND_URL = "https://second-brain-app-0m37.onrender.com"
